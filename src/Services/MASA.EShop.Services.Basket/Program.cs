49a791e4182b4e0579d57f7fd496f67a2902e3e6
zhenlei520 <wangzhenlei520@gmail.com>
2021-09-27 19:19:49 +0800

    chore: update MASA.Contribs package library

--- src/Services/MASA.EShop.Services.Basket/Program.cs
+++ src/Services/MASA.EShop.Services.Basket/Program.cs
@@ -3,10 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Services
     .AddLazyWebApplication(builder)
     .AddScoped<IBasketRepository,BasketRepository>()
-    .AddUow<IntegrationEventLogContext>(options => options.UseSqlServer(""))
+    .AddUow<IntegrationEventLogContext>(options => options.UseSqlServer("Data Source=localhost;Initial Catalog=IntegrationEventLog;User ID=sa;Password=sa"))
     .AddDaprEventBus<IntegrationEventLogService>(options=> {
-        options.UseEventBus(builder.Services,AppDomain.CurrentDomain.GetAssemblies())
-        .UseEFEventLog(builder.Services);
+        options.UseEventBus(builder.Services,AppDomain.CurrentDomain.GetAssemblies());
     })
     .AddServices();
 
